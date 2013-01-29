05bcb1b3293902b2254765f50e29a6a41dd71383
Ivan Hanigan <ivan.hanigan@gmail.com>
2013-01-30 10:56:49 +1100

    change subset into to not return anything.

--- tests/test-sql_subset_into.r
+++ tests/test-sql_subset_into.r
@@ -1,8 +1,10 @@
 
+source("R/connect2postgres.r")
 source("R/sql_subset.r")
 source("R/sql_subset_into.r")
 source("R/pgListTables.r")
 
 ch <- connect2postgres('115.146.84.135', db='ewedb', user='gislibrary', p='gislibrary')
 sql_subset_into(ch, 'dbsize',into_table='temp101', select = '*', eval=T)
+sql_subset(ch, 'temp101', eval = T)
 dbSendQuery(ch, 'drop table temp101')
