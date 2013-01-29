36ba4ad36319adc18b96cf53f306dec48908a031
Ivan Hanigan <ivan.hanigan@gmail.com>
2013-01-30 10:52:06 +1100

    modify subset into

--- R/sql_subset_into.r
+++ R/sql_subset_into.r
@@ -1,19 +1,23 @@
 
-sql_subset_into <- function(conn, x, ..., into_schema = "public", into_table, eval = FALSE)
+sql_subset_into <- function(conn, x, ..., into_schema = "public", into_table, eval = FALSE, drop = TRUE)
 {
   sql <- sql_subset(ch, x=x, ..., select = '*', eval=F)
   sql <- gsub('from', paste("into ", into_schema, ".", into_table, "\nfrom ", sep = ""), sql)
   if(eval)
   {
     exists <- pgListTables(conn, into_schema, into_table)
-    if(nrow(exists) > 0)
+    if(nrow(exists) > 0 & drop)
       {
         dbSendQuery(conn, paste("drop table ", into_schema, ".",
                                 into_table, sep =""))
-      }
+      } else if (nrow(exists) > 0 & !drop)
+        {
+          stop("Table exists. Aborting.")
+        }
+  
     dbSendQuery(conn, sql)
-    dat <- dbGetQuery(conn, paste("select * from ", into_schema, ".", into_table, sep = ""))
-    return(dat)
+    #dat <- dbGetQuery(conn, paste("select * from ", into_schema, ".", into_table, sep = ""))
+    #return(dat)
   } else {
     return(sql)
   }
