ee3d01f2488e817eb9e6002f60d259a9189e18ba
Ivan <ivan.hanigan@gmail.com>
2013-02-11 11:40:21 +1100

    changed pglisttables to sql version

--- R/pgListTables.r
+++ R/pgListTables.r
@@ -3,13 +3,29 @@
 # name:pgListTables
 pgListTables <- function(conn, schema, pattern = NA)
 {
-  tables <- dbGetQuery(conn, "select   c.relname, nspname
+  if(!is.na(pattern))
+  {
+    tables <- dbGetQuery(conn, 
+                         paste("select   c.relname, nspname
                        FROM pg_catalog.pg_class c
                        LEFT JOIN pg_catalog.pg_namespace n
                        ON n.oid = c.relnamespace
-                       where c.relkind IN ('r','','v') ")
-  tables <- tables[grep(schema,tables$nspname),]
-  if(!is.na(pattern)) tables <- tables[grep(pattern, tables$relname),]
+                       where (c.relkind IN ('r','','v')) 
+                        and (nspname = '",schema,"' and relname = '",pattern,"')", sep = "")
+    )    
+  } else {
+    tables <- dbGetQuery(conn, 
+                         paste("select   c.relname, nspname
+                       FROM pg_catalog.pg_class c
+                       LEFT JOIN pg_catalog.pg_namespace n
+                       ON n.oid = c.relnamespace
+                       where (c.relkind IN ('r','','v')) 
+                        and (nspname = '",schema,"')", sep = "")
+    )    
+  }
+ 
+#   tables <- tables[grep(schema,tables$nspname),]
+#    tables <- tables[grep(pattern, tables$relname),]
   tables <- tables[order(tables$relname),]
   return(tables)
 }
