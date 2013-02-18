c242034b67c55a3f46b00e9784629a738873532e
Ivan Hanigan <ivan.hanigan@gmail.com>
2013-02-18 11:18:32 +1100

    fix raster

--- R/load2postgres_raster.r
+++ R/load2postgres_raster.r
@@ -1,16 +1,17 @@
 
 ################################################################
 # name:load2postgres_raster
-load2postgres_raster <- function(filename, ipaddress = "115.146.84.135", u = "gislibrary", d = 'ewedb', pgisutils = "", srid = 4283, remove = TRUE)
+load2postgres_raster <- function(filename, out_schema, out_table,  ipaddress = "115.146.84.135", u = "gislibrary", d = 'ewedb', pgisutils = "", srid = 4283, remove = TRUE)
 {
-  outname <- gsub('.tif',"", filename)
-  outname <- substr(outname, 1, nchar(outname) - 8)
+  # outname <- gsub('.tif',"", filename)
+  # outname <- substr(outname, 1, nchar(outname) - 8)
+  outname <- paste(out_schema, out_table, sep = ".")
   os <- LinuxOperatingSystem()
   if(os)
   {
    system(
   #        cat
-          paste(pgisutils,"raster2pgsql -s ",srid," -I -C -M ",filename," -F awap_grids.",outname," > ",outname,".sql", sep="")
+          paste(pgisutils,"raster2pgsql -s ",srid," -I -C -M ",filename," -F ",outname," > ",outname,".sql", sep="")
           )
 
    system(
@@ -19,7 +20,7 @@ load2postgres_raster <- function(filename, ipaddress = "115.146.84.135", u = "gi
             sep = ""))
   } else {
     sink('raster2sql.bat')
-    cat(paste(pgisutils,"raster2pgsql\" -s ",srid," -I -C -M ",filename," -F awap_grids.",outname," > ",outname,".sql\n",sep=""))
+    cat(paste(pgisutils,"raster2pgsql\" -s ",srid," -I -C -M ",filename," -F ",outname," > ",outname,".sql\n",sep=""))
 
     cat(
     paste(pgisutils,"psql\" -h ",ipaddress," -U ",u," -d ",d," -f ",outname,".sql",
