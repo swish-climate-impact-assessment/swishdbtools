900dba7557116342b2757416b7de5ba4720a4d54
Ivan Hanigan <ivan.hanigan@gmail.com>
2013-02-20 12:33:48 +1100

    mod extract raster

--- R/postgis_raster_extract.r
+++ R/postgis_raster_extract.r
@@ -3,7 +3,24 @@
 
 postgis_raster_extract <- function(conn, x, y, fun = NA, eval = FALSE, zone_label, value_label = NA, into = FALSE)
 {
+# assumptions
 out_schema <- "public"
+
+if(is.data.frame(y))
+  {
+    # assume location is from gGeoCode2, dataframe with address,  lat, long
+    # assume gda94
+    srid <- 4283
+y <- paste("(
+select cast('",y$address,"' as text ) as location, st_GeomFromText(
+                     'POINT('||
+                     ",y$long," ||
+                     ' '||
+                     ",y$lat," ||')'
+                     ,",srid,") as the_geom
+)", sep = "")
+  }
+
 #                   into ",value_label,"_join_", pwcName,
 sql <- paste("SELECT pt.",zone_label,", cast('",x,"' as text) as raster_layer,
 ST_Value(rt.rast, pt.the_geom) as ",value_label,
@@ -22,14 +39,14 @@ if(into)
   {
     sql <- gsub("\nFROM", paste("\nINTO ", out_schema, ".", out_table , "\nFROM ", sep = ""), sql)
   } else {
-    sql_insert <- paste("INSERT INTO ", out_schema, ".", out_table, " (", zone_label, ", raster_layer, ", value_label, ")", sep = "")  
+    sql_insert <- paste("INSERT INTO ", out_schema, ".", out_table, " (", zone_label, ", raster_layer, ", value_label, ")", sep = "")
     sql <- paste(sql_insert, sql, sep = "\n")
   }
 
 }
 
-return(sql)  
+return(sql)
 #dbSendQuery(conn,
-#    
+#
 
 }
