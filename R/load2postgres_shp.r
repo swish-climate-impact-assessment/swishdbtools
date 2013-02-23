ed101740afd4835be5a1b389a08af08ebe07b709
Ivan Hanigan <ivan.hanigan@gmail.com>
2013-02-23 12:09:55 +1100

    shp

--- R/load2postgres_shp.r
+++ R/load2postgres_shp.r
@@ -1,37 +1,49 @@
 
 ################################################################
 
-load2postgres_shp <- function(filename, out_schema, out_table,  ipaddress = "115.146.84.135", u = "gislibrary", d = 'ewedb', pgisutils = "", srid = 4283, remove = TRUE)
-{
-  outname <- paste(out_schema, out_table, sep = ".")
-  os <- LinuxOperatingSystem()
-  if(os)
+  load2postgres_shp <- function(filename, out_schema, out_table,  ipaddress = "115.146.84.135", u = "gislibrary", d = 'ewedb', pgisutils = "", srid = 4283, remove = TRUE, eval = FALSE)
   {
-   system(
-  #        cat
-          paste(pgisutils,"shp2pgsql -s ",srid," -D ",filename," ",outname," > ",outname,".sql", sep="")
-          )
-
-   system(
-  #        cat
-          paste("psql -h ",ipaddress," -U ",u," -d ",d," -f ",outname,".sql",
-            sep = ""))
-  } else {
-    sink('shp2sql.bat')
-    cat(paste(pgisutils,"shp2pgsql\" -s ",srid," -D ",filename," ",outname," > ",outname,".sql\n",sep=""))
-
-    cat(
-    paste(pgisutils,"psql\" -h ",ipaddress," -U ",u," -d ",d," -f ",outname,".sql",
-    sep = "")
-      )
-    sink()
-    system('shp2sql.bat')
-    file.remove('shp2sql.bat')
-  }
-
-  if(remove)
+    outname <- paste(out_schema, out_table, sep = ".")
+    os <- LinuxOperatingSystem()
+    if(os)
     {
-      file.remove(filename)
-      file.remove(paste(outname, '.sql', sep =""))
+#      system(
+    #        cat
+           sql <- paste(pgisutils,"shp2pgsql -s ",srid," -D ",filename," ",outname," > ",outname,".sql", sep="")
+            #)
+
+     #system(
+    #        cat
+            cli <- paste("psql -h ",ipaddress," -U ",u," -d ",d," -f ",outname,".sql",
+              sep = "")
+            #)
+    if(eval)
+      {
+        system(sql)
+        system(cli)
+        file.remove(paste(outname, '.sql', sep =""))
+      } else {
+        sql <- paste(sql, cli, sep = "\n")
+        return(sql)
+      }
+
+
+    } else {
+      sink('shp2sql.bat')
+      cat(paste(pgisutils,"shp2pgsql\" -s ",srid," -D ",filename," ",outname," > ",outname,".sql\n",sep=""))
+
+      cat(
+      paste(pgisutils,"psql\" -h ",ipaddress," -U ",u," -d ",d," -f ",outname,".sql",
+      sep = "")
+        )
+      sink()
+      system('shp2sql.bat')
+      file.remove('shp2sql.bat')
     }
-}
+
+    if(remove)
+      {
+        file.remove(filename)
+
+      }
+  }
