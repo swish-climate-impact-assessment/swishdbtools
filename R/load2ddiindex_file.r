8a18a6d300b68263a6fcfe024558943a21c0cc61
Ivan Hanigan <ivan.hanigan@gmail.com>
2013-03-15 22:32:09 +1100

    minor fix to the load ddi file

--- R/load2ddiindex_file.r
+++ R/load2ddiindex_file.r
@@ -1,27 +1,28 @@
 
 ################################################################
 # name:load2ddiindex_file
-  
+
   load2ddiindex_file <- function(conn=NA, filedscr)
   {
-  
-  
-  
+
     if(exists('filedscr'))
       {
       fileexists <- dbGetQuery(conn,
                   paste("select * from filedscr where filename = '",filedscr$FILENAME,"' and filelocation ='",filedscr$FILELOCATION,"'", sep="")
                   )
       if(nrow(fileexists) > 0) stop('File record already exists.')
-
-      filedscr$PRODDATEDOCFILE <- format(as.Date( substr(filedscr$PRODDATEDOCFILE,1,10),'%Y-%m-%d'),"%d/%b/%Y")
-      
+      if(length(grep("PRODDATEDOCFILE", names(filedscr))) > 0)
+        {
+          filedscr$PRODDATEDOCFILE <- format(as.Date(
+                                      substr(filedscr$PRODDATEDOCFILE,1,10),'%Y-%m-%d'
+                                      ),"%d/%b/%Y")
+        }
     dbSendUpdate(ch,
     # cat(
       paste('
       insert into FILEDSCR (',paste(names(filedscr), sep = '', collapse = ', '),')
       VALUES (',paste("'",paste(gsub("'","",ifelse(is.na(filedscr),'',filedscr)),sep='',collapse="', '"),"'",sep=''),')',sep='')
     )
-  
+
       }
   }
