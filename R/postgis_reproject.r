24231211ca8a39af07ddb1e2f3917329cdd21508
ivanhanigan <ivanhanigan@gmail.com>
2013-04-10 22:03:51 +1000

    add tangled

---
+++ R/postgis_reproject.r
@@ -0,0 +1,46 @@
+
+################################################################
+# name:postgis_reproject
+postgis_reproject <- function(conn, x, srid, out_table = NA, eval = F)
+  {
+    if(length(grep("\\.",x)) == 0)
+      {
+        schema <- "public"
+        table <- x
+      } else {
+        schema <- strsplit(x, "\\.")[[1]][1]
+        table <- strsplit(x, "\\.")[[1]][2]
+      }
+    if(is.na(out_table))
+      {
+        out_table <- paste(table,"_reprojected", sep = "")
+      }
+
+    namesList <- names(sql_subset(conn, paste(schema, table, sep = "."), eval = T))
+    namesList <- c(namesList[-c(which(namesList == "gid"))], "geom")
+
+    sql <- sql_subset_into(
+                           conn,
+                           paste(schema,".",table,sep=""),
+                           select = paste(
+                             gsub("geom", sprintf("st_transform(geom, %s) as geom", srid), namesList),
+                             sep = "", collapse = ","
+                             ),
+                           into_schema = schema,
+                           into_table = out_table,
+                           eval = F
+                           )
+
+    sql <- paste(sql,
+                 sprintf("alter table %s add column gid serial primary key",out_table),
+                 sep = "\n"
+                 )
+
+    if(eval == FALSE)
+      {
+        return(sql)
+      } else {
+        dbSendQuery(conn, sql)
+      }
+
+  }
