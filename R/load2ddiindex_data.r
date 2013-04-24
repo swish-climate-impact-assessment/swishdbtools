45e8b49ab37a21674e39342564e65319bc4347a7
ivanhanigan <ivanhanigan@gmail.com>
2013-04-24 14:55:12 +1000

    mod to conn in load2ddiindex_data

--- R/load2ddiindex_data.r
+++ R/load2ddiindex_data.r
@@ -13,17 +13,17 @@ load2ddiindex_data <- function(conn, filepath, datadscr)
       table <- strsplit(filepath, "\\.")[[1]][2]
     }
 
-sql <- sql_subset(ch, 'ddiindexdb.filedscr',
+sql <- sql_subset(conn, 'ddiindexdb.filedscr',
                   subset = paste("filelocation = '",schema,"' and filename = '",table,"'", sep =""),
                   eval=F, check =F)
 #cat(sql)
-file <- dbGetQuery(ch, sql)
+file <- dbGetQuery(conn, sql)
 fid <- file$FILEID
 #fid
 datadscr$FILEID <- fid
 for(i in 1:nrow(datadscr))
   {
-  dbSendUpdate(ch,
+  dbSendUpdate(conn,
     paste('
     insert into DATADSCR (',paste(names(datadscr), sep = '', collapse = ', '),')
     VALUES (',paste("'",paste(gsub("'","",ifelse(is.na(datadscr[i,]),'',datadscr[i,])),sep='',collapse="', '"),"'",sep=''),')',sep='')
