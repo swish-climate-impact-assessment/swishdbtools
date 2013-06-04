3672f29e6683533c37a6380d33445843a5eeaa6f
ivanhanigan <ivan.hanigan@gmail.com>
2013-06-04 15:34:22 +1000

    mod test

--- tests/test-read_file.r
+++ tests/test-read_file.r
@@ -4,7 +4,7 @@
 if (!require(RODBC)) install.packages('RODBC'); require(RODBC) # for
                                       # getSqlTypeInfo
 if(!require(foreign)) install.packages('foreign'); require(foreign)
-if(!require(XLConnect)) install.packages('XLConnect'); require(XLConnect)
+#if(!require(XLConnect)) install.packages('XLConnect'); require(XLConnect)
 
 require(devtools)
 install_github("swishdbtools", "swish-climate-impact-assessment")
@@ -14,5 +14,5 @@ write.csv(airquality, gsub("dta", "csv", filename), row.names=FALSE)
 # manually convert to xls
 dir("/tmp")
 df <- read_file(filename)
-df <- read_file(gsub("dta","xls",filename))
+#df <- read_file(gsub("dta","xls",filename))
 df <- read_file(gsub("dta","csv",filename))
